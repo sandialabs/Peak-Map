7bc4cb0e6eaa0b611ac130bd6dac65fdb0f6a3a4
Elliott Leonard <eleonar@sandia.gov>
2025-02-10 10:42:49 -0700

    Just a bit of cleanup ion CAMIO

--- CAMDataReader/CAMIO.cs
+++ CAMDataReader/CAMIO.cs
@@ -20,12 +20,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Diagnostics;
-using System.Data;
-using System.Collections;
-using System.ComponentModel;
+//using System.Data;
 
 namespace CAMInputOutput
 {
